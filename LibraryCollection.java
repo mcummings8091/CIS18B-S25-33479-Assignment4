87f001a87120d660e132a86d9d01a66025f4f490
Matthew Cummings <mcummings8091@gmail.com>
2025-04-25 11:24:06 -0700

    Redundancy fix

--- LibraryCollection.java
+++ LibraryCollection.java
@@ -4,7 +4,7 @@ public class LibraryCollection {
     private HashMap<String, ArrayList<Book>> genreMap;
 
     public LibraryCollection() {
-        genreMap = new HashMap<String, ArrayList<Book>>();  
+        genreMap = new HashMap<>();  
     }
 
     //Testing method
@@ -19,26 +19,11 @@ public class LibraryCollection {
     }
 
     public void addBook(Book book) {
-        // TODO: Add books to genreMap
-        // boolean genreFound = false;
         if (genreMap.containsKey(book.getGenre())) { //Search through collection for existing genre
             genreMap.get(book.getGenre()).add(book); //If the genre is found, add the book to it's genre category
         } else {
             genreMap.put(book.getGenre(), new ArrayList<>(List.of(book)));//Create a new category and place the book in it.
         }
-
-        /*
-        for (String genre : genreMap.keySet()) { //Search through collection for existing genre
-            if (book.getGenre().equals(genre)) { //If the genre is found
-                genreMap.get(genre).add(book); //Add book to it's genre category 
-                genreFound = true;
-            }
-        }
-
-        if (!genreFound) { //If the books genre wasn't found in the collection
-            genreMap.put(book.getGenre(), new ArrayList<>(List.of(book)));//Create a new category and place the book in it. 
-        }
-        */
     }
 
     public Iterator<Book> getGenreIterator(String genre) {
